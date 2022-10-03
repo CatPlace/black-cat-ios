50c7677ae75d18d4e8f6d09263ad771656c398b2
lgvv <lgw9898@naver.com>
2022-10-03 22:37:56 +0900

    [추가] 워크스페이스 작성

--- BlackCat/ViewController.swift
+++ BlackCat/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import BlackCatKEY
 
 class ViewController: UIViewController {
 
@@ -13,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = .red
+        
+        KKEETT().testKey
     }
     
 }
