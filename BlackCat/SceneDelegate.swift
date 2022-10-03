b761d5890a0a6a5f686c5cdaaba3bcd490431b8d
김지훈 <ben224@naver.com>
2022-10-03 21:54:27 +0900

    [수정] 초기 윈도우 설정

--- BlackCat/SceneDelegate.swift
+++ BlackCat/SceneDelegate.swift
@@ -13,8 +13,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        guard let _ = (scene as? UIWindowScene) else { return }
-        window?.windowScene = UIWindow(windowScene: window)
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
         window?.backgroundColor = .systemBackground
         window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
