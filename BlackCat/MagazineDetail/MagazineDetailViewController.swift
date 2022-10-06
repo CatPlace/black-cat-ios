77edd19d6f5ee846794fecb18de2b42b16cb3e90
lgvv <lgw9898@naver.com>
2022-10-06 12:50:33 +0900

    [추가] add SPM RxDataSource

---
+++ BlackCat/MagazineDetail/MagazineDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MagazineDetailViewController.swift
+//  BlackCat
+//
+//  Created by Hamlit Jason on 2022/10/06.
+//
+
+import UIKit
+import ReactorKit
+import RxCocoa
+import RxSwift
+
+
+class MagazineDetailViewController: UIViewController {
+    
+}
