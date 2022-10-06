5cb5ec5b18f34a5b79d5c03fa27c4a5d678eed7f
lgvv <lgw9898@naver.com>
2022-10-06 14:15:07 +0900

    [추가] BaseCell 구성

--- BlackCat/MagazineDetail/MagazineDetailCell/MagazineDetailTextCell.swift
+++ BlackCat/MagazineDetail/MagazineDetailCell/MagazineDetailTextCell.swift
@@ -5,34 +5,63 @@
 //  Created by Hamlit Jason on 2022/10/06.
 //
 
-//import UIKit
-//import ReactorKit
-//
-//class MagazineDetailTextCell: UITableViewCell, View {
-//    static let identifier = String(describing: MagazineDetailTextCell.self)
-//
-//    typealias Reactor = MagazineDetailTextCellReactor
-//    var disposeBag: DisposeBag = DisposeBag()
-//    
-//    func bind(reactor: Reactor) {
-//        
-//    }
-//    
-//    let textLabel = UILabel()
-//    
-//}
-//
-//class MagazineDetailTextCellReactor: Reactor {
-//
-//    typealias Action = NoAction
-//
-//    struct State {
-//
-//    }
-//
-//    let initialState = State()
-//
-//    init(initialState: State = State()) {
-//        self.initialState = initialState
-//    }
-//}
+import UIKit
+import ReactorKit
+import SnapKit
+
+class MagazineDetailBaseCell: UITableViewCell {
+    var disposeBag: DisposeBag = DisposeBag()
+    
+    // MARK: - Initializing
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        initialize()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func initialize() {
+        self.setUI()
+    }
+    
+    func setUI() { /* override setUI */ }
+}
+
+class MagazineDetailTextCell: MagazineDetailBaseCell, View {
+    static let identifier = String(describing: MagazineDetailTextCell.self)
+    typealias Reactor = MagazineDetailTextCellReactor
+    
+    // MARK: - Binding
+    func bind(reactor: Reactor) {
+        
+    }
+    
+    override func setUI() {
+        addSubview(contentTextLabel)
+        
+        contentTextLabel.numberOfLines = 0
+        contentTextLabel.text = "도라에몽"
+        contentTextLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(10)
+        }
+    }
+    
+    // MARK: - UIComponents
+    private let contentTextLabel = UILabel()
+}
+
+class MagazineDetailTextCellReactor: Reactor {
+
+    typealias Action = NoAction
+
+    struct State { }
+
+    var initialState = State()
+
+    init(initialState: State = State()) {
+        self.initialState = initialState
+    }
+}
