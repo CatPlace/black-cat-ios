6608cb255fe55fe7b827b543fbc29f60fb40cabe
lgvv <lgw9898@naver.com>
2022-10-08 14:47:53 +0900

    [추가] CALayer+, UITabBar+

---
+++ BlackCat/Extension/UITabBar+.swift
@@ -0,0 +1,17 @@
+//
+//  UITabBar.swift
+//  BlackCat
+//
+//  Created by Hamlit Jason on 2022/10/08.
+//
+
+import UIKit
+
+extension UITabBar {
+    // 기본 그림자 스타일을 초기화해야 커스텀 스타일을 적용할 수 있다.
+    static func clearShadow() {
+        UITabBar.appearance().shadowImage = UIImage()
+        UITabBar.appearance().backgroundImage = UIImage()
+        UITabBar.appearance().backgroundColor = UIColor.white
+    }
+}
