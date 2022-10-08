b42052a7709977fde749f420c525a12ca9feff59
SeYeongYoon <ssyoun4092@naver.com>
2022-10-08 13:25:18 +0900

    [수정] 네이밍 수정: HomeCompositionalViewController -> HomeViewController

--- BlackCat/Home/HomeViewController.swift
+++ BlackCat/Home/HomeViewController.swift
@@ -2,42 +2,110 @@
 //  HomeViewController.swift
 //  BlackCat
 //
-//  Created by SeYeong on 2022/10/05.
+//  Created by SeYeong on 2022/10/06.
 //
 
 import UIKit
 
 import RxCocoa
+import RxDataSources
 import RxSwift
 import SnapKit
 
 class HomeViewController: UIViewController {
 
     // MARK: - Properties
-    let disposeBag = DisposeBag()
+
     let viewModel = HomeViewModel()
+    let disposeBag = DisposeBag()
+    private lazy var dataSource = RxCollectionViewSectionedReloadDataSource<HomeSection>(
+        configureCell: { dataSource, collectionView, indexPath, item in
+        switch item {
+        case .HomeCategoryCellItem(let category):
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: HomeCategoryCell.identifier,
+                for: indexPath
+            ) as! HomeCategoryCell
+
+            cell.bind(to: HomeCategoryCellViewModel(category: category))
+
+            return cell
+
+        case .Section1(let section1):
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: HomeSection1Cell.identifier,
+                for: indexPath
+            ) as! HomeSection1Cell
+
+            cell.bind(to: HomeSection1CellViewModel(section1: section1))
+
+            return cell
+
+        case .Empty(let empty):
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: HomeEmptyCell.identifier,
+                for: indexPath
+            ) as! HomeEmptyCell
+
+            return cell
+
+        case .Section2(let section2):
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: HomeSection2Cell.identifer,
+                for: indexPath
+            ) as! HomeSection2Cell
+
+            cell.bind(to: HomeSection2CellViewModel(section2: section2))
+
+            return cell
+        }
+    }, configureSupplementaryView: { dataSource, collectionView, kind, indexPath -> UICollectionReusableView in
+        guard kind == UICollectionView.elementKindSectionHeader,
+              indexPath.section == 1 || indexPath.section == 3
+        else {
+            return UICollectionReusableView()
+        }
+
+        let headerView = collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: HomeHeaderView.identifer,
+            for: indexPath
+        ) as! HomeHeaderView
+        let headerTitle = dataSource.sectionModels[indexPath.section].header
+
+        headerView.titleLabel.text = headerTitle
+
+        return headerView
+    })
 
     // MARK: - Binding
 
-    private func bind(to viewModel: HomeViewModel) {
-//        viewModel.categoryItems
-//            .drive(categoryCollectionView.rx.items(
-//                cellIdentifier: HomeCategoryCell.identifier,
-//                cellType: HomeCategoryCell.self)
-//            ) { index, title, cell in
-//                cell.categoryTitleLabel.text = title
-//                //            cell.bind(to: viewModel)
-//            }
-//            .disposed(by: disposeBag)
-//
-//        viewModel.categoryItems
-//            .drive(section1CollectionView.rx.items(
-//                cellIdentifier: HomeSection1Cell.identifier,
-//                cellType: HomeSection1Cell.self)
-//            ) { index, title, cell in
-//                cell.producerLabel.text = title
-//            }
-//            .disposed(by: disposeBag)
+    private func bind() {
+        searchBarButtonItem.rx.tap
+            .bind(to: viewModel.didTapSearchBarButtonItem)
+            .disposed(by: disposeBag)
+
+        heartBarButtonItem.rx.tap
+            .bind(to: viewModel.didTapHeartBarButtonItem)
+            .disposed(by: disposeBag)
+
+        viewModel.homeItems
+            .drive(homeCollectionView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+    }
+
+    // MARK: - Initializing
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+
+        rx.viewDidLoad
+            .bind(to: viewModel.viewDidLoad)
+            .disposed(by: disposeBag)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
     // MARK: - Life Cycle
@@ -47,7 +115,7 @@ class HomeViewController: UIViewController {
 
         setNavigationBar()
         setUI()
-        bind(to: viewModel)
+        bind()
     }
 
     // MARK: - UIComponents
@@ -59,134 +127,195 @@ class HomeViewController: UIViewController {
 
         return UIBarButtonItem(customView: label)
     }()
-    let searchBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "magnifyingglass"), style: .plain, target: self, action: nil)
-    let heartBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "heart"), style: .plain, target: self, action: nil)
 
-    let scrollView = UIScrollView()
-    let contentView = UIView()
-    let VStackView = UIStackView()
-    
-    lazy var categoryCollectionView = UICollectionView(
-        frame: .zero,
-        collectionViewLayout: categoryCollectionViewFlowLayout
-    )
-    let categoryCollectionViewFlowLayout: UICollectionViewLayout = {
-        let spacing: CGFloat = 12
-        let itemWidth = (UIScreen.main.bounds.width - (spacing * 4) - 28) / 5
+    let searchBarButtonItem: UIBarButtonItem = {
+        let image = UIImage(systemName: "magnifyingglass")?.withTintColor(.black, renderingMode: .alwaysOriginal)
 
-        let layout = UICollectionViewFlowLayout()
-        layout.minimumLineSpacing = spacing
-        layout.minimumInteritemSpacing = spacing
-        layout.itemSize = CGSize(width: itemWidth, height: itemWidth)
-        layout.sectionInset = UIEdgeInsets(top: 30, left: 14, bottom: 30, right: 14)
+        return UIBarButtonItem(image: image, style: .plain, target: self, action: nil)
+    }()
+
+    let heartBarButtonItem: UIBarButtonItem = {
+        let image = UIImage(systemName: "heart")?.withTintColor(.black, renderingMode: .alwaysOriginal)
+
+        return UIBarButtonItem(image: image, style: .plain, target: self, action: nil)
+    }()
+
+    lazy var homeCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: compositionalLayout
+        )
+
+        collectionView.register(
+            HomeCategoryCell.self,
+            forCellWithReuseIdentifier: HomeCategoryCell.identifier
+        )
+        collectionView.register(
+            HomeSection1Cell.self,
+            forCellWithReuseIdentifier: HomeSection1Cell.identifier
+        )
+        collectionView.register(
+            HomeEmptyCell.self,
+            forCellWithReuseIdentifier: HomeEmptyCell.identifier
+        )
+        collectionView.register(
+            HomeSection2Cell.self,
+            forCellWithReuseIdentifier: HomeSection2Cell.identifer
+        )
+        collectionView.register(
+            HomeHeaderView.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: HomeHeaderView.identifer
+        )
+
+        collectionView.showsVerticalScrollIndicator = false
+
+        return collectionView
+    }()
+
+    let compositionalLayout: UICollectionViewLayout = {
+        let layout = UICollectionViewCompositionalLayout { section, env -> NSCollectionLayoutSection? in
+            switch section {
+            case 0:
+                let itemSpacing: CGFloat = 12
+                let sectionLeadingInset: CGFloat = 14
+                let sectionTrailinginset: CGFloat = 14
+                let itemWidth = (UIScreen.main.bounds.width - (itemSpacing * 4) - (sectionLeadingInset + sectionTrailinginset)) / 5
+
+                let itemSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .fractionalHeight(1)
+                )
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+                let groupSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .absolute(itemWidth)
+                )
+
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 5)
+                group.interItemSpacing = .fixed(itemSpacing)
+
+                // Section
+                let section = NSCollectionLayoutSection(group: group)
+                section.interGroupSpacing = itemSpacing
+                section.contentInsets = NSDirectionalEdgeInsets(top: 30, leading: 14, bottom: 30, trailing: 14)
+
+                let sectionBackgroundView = NSCollectionLayoutDecorationItem.background(
+                    elementKind: HomeCategorySectionBackgroundReusableView.identifier
+                )
+                section.decorationItems = [sectionBackgroundView]
+
+                return section
+            case 1:
+                let itemSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .fractionalHeight(1)
+                )
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+                let groupSize = NSCollectionLayoutSize(
+                    widthDimension: .estimated(140),
+                    heightDimension: .estimated(210)
+                )
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .continuous
+                section.interGroupSpacing = 20
+                section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 0, trailing: 20)
+
+                let headerSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .estimated(43)
+                )
+                let header = NSCollectionLayoutBoundarySupplementaryItem(
+                    layoutSize: headerSize,
+                    elementKind: UICollectionView.elementKindSectionHeader,
+                    alignment: .top
+                )
+                header.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: -10, bottom: 0, trailing: 0)
+
+                section.boundarySupplementaryItems = [header]
+
+                return section
+
+            case 2:
+                let itemSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .fractionalHeight(1)
+                )
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+                let groupSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .absolute(20)
+                )
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+                let section = NSCollectionLayoutSection(group: group)
+
+                return section
+
+            default:
+                let itemSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .fractionalHeight(1)
+                )
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+                let groupSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .fractionalWidth(1 / 3)
+                )
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 3)
+                group.interItemSpacing = .fixed(1)
+
+                let section = NSCollectionLayoutSection(group: group)
+                section.interGroupSpacing = 1
+                section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0.5, bottom: 0, trailing: 0.5)
+
+                let headerSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .estimated(43)
+                )
+                let header = NSCollectionLayoutBoundarySupplementaryItem(
+                    layoutSize: headerSize,
+                    elementKind: UICollectionView.elementKindSectionHeader,
+                    alignment: .top
+                )
+                header.contentInsets = .init(top: 0, leading: 9.5, bottom: 0, trailing: 0)
+
+                section.boundarySupplementaryItems = [header]
+
+                return section
+            }
+        }
+
+        layout.register(
+            HomeCategorySectionBackgroundReusableView.self,
+            forDecorationViewOfKind: HomeCategorySectionBackgroundReusableView.identifier
+        )
 
         return layout
     }()
-
-    let section1View = UIView()
-    let section1TitleLabel = UILabel()
-    lazy var section1CollectionView = UICollectionView(
-        frame: .zero,
-        collectionViewLayout: section1CollectionViewFlowLayout
-    )
-
-    let section1CollectionViewFlowLayout: UICollectionViewLayout = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.minimumLineSpacing = 0
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
-        layout.itemSize = CGSize(width: 160, height: 217)
-
-        return layout
-    }()
-
-    let dividerView = UIView()
-
-    private func categoryCellHeight() -> CGFloat {
-        let spacing: CGFloat = 12
-        let topInset: CGFloat = 30
-        let bottomInset: CGFloat = 30
-
-        return (UIScreen.main.bounds.width - (spacing * 2) - (topInset + bottomInset)) / 3
-    }
 }
 
 extension HomeViewController {
     private func setNavigationBar() {
-        navigationController?.navigationBar.backgroundColor = .white
-        navigationController?.navigationBar.layer.shadowColor = UIColor.black.cgColor
-        navigationController?.navigationBar.layer.shadowOffset = CGSize(width: 0.0, height: 10.0)
-        navigationController?.navigationBar.layer.shadowRadius = 5.0
-        navigationController?.navigationBar.layer.shadowOpacity = 0.05
-        navigationController?.navigationBar.layer.masksToBounds = false
         navigationItem.leftBarButtonItem = leftTitleBarButtonItem
-        navigationItem.rightBarButtonItems = [searchBarButtonItem, heartBarButtonItem]
+        navigationItem.rightBarButtonItems = [
+            heartBarButtonItem,
+            searchBarButtonItem
+        ]
     }
 
     private func setUI() {
-        view.addSubview(scrollView)
+        view.addSubview(homeCollectionView)
 
-        scrollView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-        }
-
-        scrollView.addSubview(contentView)
-
-        contentView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-            $0.width.equalToSuperview()
-        }
-
-        contentView.addSubview(VStackView)
-
-        VStackView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-        }
-
-        [categoryCollectionView, section1View, dividerView].forEach { VStackView.addArrangedSubview($0) }
-
-        VStackView.axis = .vertical
-        VStackView.spacing = 0
-
-        categoryCollectionView.snp.makeConstraints {
-            let cellHeight = (UIScreen.main.bounds.width - (12 * 4) - 28) / 5
-            $0.height.equalTo(cellHeight * 3 + (12 * 2) + 60)
-        }
-        categoryCollectionView.backgroundColor = .designSystem(.BackgroundSecondary)
-
-        categoryCollectionView.register(
-            HomeCategoryCell.self,
-            forCellWithReuseIdentifier: HomeCategoryCell.identifier
-        )
-
-        section1View.snp.makeConstraints {
-            $0.height.equalTo(260)
-        }
-
-        [section1TitleLabel, section1CollectionView].forEach { section1View.addSubview($0) }
-
-        section1TitleLabel.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(15)
-            $0.leading.equalToSuperview().inset(20)
-        }
-
-        section1TitleLabel.text = "항목1"
-        section1TitleLabel.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 16)
-
-        section1CollectionView.snp.makeConstraints {
-            $0.top.equalTo(section1TitleLabel.snp.bottom)
+        homeCollectionView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.leading.trailing.bottom.equalToSuperview()
         }
-
-        section1CollectionView.register(
-            HomeSection1Cell.self,
-            forCellWithReuseIdentifier: HomeSection1Cell.identifier
-        )
-        section1CollectionView.showsHorizontalScrollIndicator = false
-
-        dividerView.snp.makeConstraints {
-            $0.height.equalTo(20)
-        }
-        dividerView.backgroundColor = .designSystem(.BackgroundSecondary)
     }
 }
--- BlackCat/SceneDelegate.swift
+++ BlackCat/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: windowScene)
         window?.backgroundColor = .systemBackground
-        window?.rootViewController = UINavigationController(rootViewController: HomeCompositionalViewController())
+        window?.rootViewController = UINavigationController(rootViewController: HomeViewController())
         window?.makeKeyAndVisible()
     }
 
