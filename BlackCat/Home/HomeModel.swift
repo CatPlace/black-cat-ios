a98d0447e92f89b6388892c923ea7df1f4914daf
SeYeongYoon <ssyoun4092@naver.com>
2022-10-08 13:26:01 +0900

    [추가] Home Model 파일 추가(SDK로 옮길 모델) 및 HomeSectiion을 HomeViewModel 파일 하단으로 위치

---
+++ BlackCat/Home/HomeModel.swift
@@ -0,0 +1,44 @@
+//
+//  HomeModel.swift
+//  BlackCat
+//
+//  Created by SeYeong on 2022/10/08.
+//
+
+import Foundation
+
+// TODO: - SDK로 옮겨야 됨!
+
+struct HomeCategory {
+    let title: String
+
+    static let `default`: [HomeCategory] = [
+        HomeCategory(title: "전체보기"),
+        HomeCategory(title: "레터링"),
+        HomeCategory(title: "미니 타투"),
+        HomeCategory(title: "감성 타투"),
+        HomeCategory(title: "이레즈미"),
+        HomeCategory(title: "블랙&그레이"),
+        HomeCategory(title: "라인워크"),
+        HomeCategory(title: "헤나"),
+        HomeCategory(title: "커버업"),
+        HomeCategory(title: "뉴스쿨"),
+        HomeCategory(title: "올드스쿨"),
+        HomeCategory(title: "잉크 스플래쉬"),
+        HomeCategory(title: "치카노"),
+        HomeCategory(title: "컬러"),
+        HomeCategory(title: "캐릭터")
+    ]
+}
+
+struct Section1 {
+    let imageURLString: String
+    let priceString: String
+    let tattooistName: String
+}
+
+struct Empty {}
+
+struct Section2 {
+    let imageURLString: String
+}
--- BlackCat/Home/HomeViewModel.swift
+++ BlackCat/Home/HomeViewModel.swift
@@ -11,52 +11,6 @@ import RxCocoa
 import RxDataSources
 import RxSwift
 
-struct HomeCategory {
-    let title: String
-
-    static let `default`: [HomeCategory] = [
-        HomeCategory(title: "전체보기"),
-        HomeCategory(title: "레터링"),
-        HomeCategory(title: "미니 타투"),
-        HomeCategory(title: "감성 타투"),
-        HomeCategory(title: "이레즈미"),
-        HomeCategory(title: "블랙&그레이"),
-        HomeCategory(title: "라인워크"),
-        HomeCategory(title: "헤나"),
-        HomeCategory(title: "커버업"),
-        HomeCategory(title: "뉴스쿨"),
-        HomeCategory(title: "올드스쿨"),
-        HomeCategory(title: "잉크 스플래쉬"),
-        HomeCategory(title: "치카노"),
-        HomeCategory(title: "컬러"),
-        HomeCategory(title: "캐릭터")
-    ]
-}
-
-struct Section1 {
-    let imageURLString: String
-    let priceString: String
-    let tattooistName: String
-}
-
-struct Empty {}
-
-struct Section2 {
-    let imageURLString: String
-}
-
-struct HomeSection {
-    var header: String = ""
-    var items: [Item]
-}
-
-enum HomeItem {
-    case HomeCategoryCellItem(HomeCategory)
-    case Section1(Section1)
-    case Empty(Empty)
-    case Section2(Section2)
-}
-
 class HomeViewModel {
 
     // MARK: - Properties
@@ -115,4 +69,25 @@ class HomeViewModel {
             .dispose()
     }
 
+}
+
+struct HomeSection {
+    var header: String = ""
+    var items: [Item]
+
+    enum HomeItem {
+        case HomeCategoryCellItem(HomeCategory)
+        case Section1(Section1)
+        case Empty(Empty)
+        case Section2(Section2)
+    }
+}
+
+extension HomeSection: SectionModelType {
+    typealias Item = HomeItem
+
+    init(original: HomeSection, items: [Item] = []) {
+        self = original
+        self.items = items
+    }
 }
