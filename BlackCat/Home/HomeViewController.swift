5bfe59017bbbe899327e24459c91c6be1bcb60ab
SeYeongYoon <ssyoun4092@naver.com>
2022-10-10 23:48:25 +0900

    [수정] viewDidLoad 로직 init으로 옮김

--- BlackCat/Home/HomeViewController.swift
+++ BlackCat/Home/HomeViewController.swift
@@ -95,19 +95,12 @@ class HomeViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
 
         bind()
+        setNavigationBar()
+        setUI()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-
-    // MARK: - Life Cycle
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        setNavigationBar()
-        setUI()
     }
 
     // MARK: - UIComponents
