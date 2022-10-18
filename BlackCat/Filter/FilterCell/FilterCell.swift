d307b06335035c6dbb03072d618e3631b0dd3488
lgvv <lgw9898@naver.com>
2022-10-19 08:32:01 +0900

    [수정] 불필요한 코드 삭제 및 개선

--- BlackCat/Filter/FilterCell/FilterCell.swift
+++ BlackCat/Filter/FilterCell/FilterCell.swift
@@ -59,13 +59,8 @@ final class FilterCell: FilterBaseCell {
         }
     }
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    override func initialize() {
         setUI()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
     private func setUI() {
