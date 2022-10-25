c68db0357ee43c3175210c150d983bdff9614604
SeYeongYoon <ssyoun4092@naver.com>
2022-10-25 11:44:58 +0900

    [추가] 이미지로만 가득 찬 CommonFullImageCell, viewModel 추가

---
+++ BlackCat/Commons/Cells/CommonFullImageCellViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  CommonFullImageCellViewModel.swift
+//  BlackCat
+//
+//  Created by SeYeong on 2022/10/25.
+//
+
+import Foundation
+
+class CommonFullImageCellViewModel {
+    let imageURLString: String
+
+    init(imageURLString: String) {
+        self.imageURLString = imageURLString
+    }
+}
