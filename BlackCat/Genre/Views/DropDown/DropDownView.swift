2f7b3ae5c68626bbe654b99877a74e3316516cda
SeYeongYoon <ssyoun4092@naver.com>
2022-11-02 11:01:33 +0900

    [추가] UIView+ 파일 추가 및 globalFrame 추가

--- BlackCat/Genre/Views/DropDown/DropDownView.swift
+++ BlackCat/Genre/Views/DropDown/DropDownView.swift
@@ -237,10 +237,3 @@ extension Reactive where Base: DropDownView {
         return base.tableView.rx.itemSelected
     }
 }
-
-extension UIView {
-    var globalFrame: CGRect? {
-        let rootView = UIApplication.shared.keyWindow?.rootViewController?.view
-        return self.superview?.convert(self.frame, to: rootView)
-    }
-}
