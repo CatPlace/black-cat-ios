29c1674df5d9ac687ce356d59f28a70f92a4fc5b
SeYeongYoon <ssyoun4092@naver.com>
2022-11-24 12:17:12 +0900

    [추가] BookmarkMagazineViewController UI 추가

--- BlackCat/Bookmark/BookmarkMagazineViewController.swift
+++ BlackCat/Bookmark/BookmarkMagazineViewController.swift
@@ -7,21 +7,80 @@
 
 import UIKit
 
+import BlackCatSDK
+import RxCocoa
+import RxSwift
 import SnapKit
 
 class BookmarkMagazineViewController: UIViewController {
 
+    let disposeBag = DisposeBag()
+    enum Reusable {
+        static let magazineCell = ReusableCell<LastMagazineCell>()
+    }
+
+    // MARK: - Properties
+
+    let viewModel: BookmarkTattooViewModel
+
+    // MARK: - Binding
+
+    private func bind(to viewModel: BookmarkTattooViewModel) {
+    }
+
+    // MARK: - Initialize
+
+    init(viewModel: BookmarkTattooViewModel) {
+        self.viewModel = viewModel
+
+        super.init(nibName: nil, bundle: nil)
+
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.addSubview(label)
-        label.snp.makeConstraints {
-            $0.center.equalToSuperview()
-        }
+        setUI()
     }
 
-    private let label: UILabel = {
-        $0.text = "BookmarkMagazine"
-        return $0
-    }(UILabel())
+    // MARK: - UIComponents
+
+    private lazy var collectionView: UICollectionView = {
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+        cv.register(Reusable.magazineCell)
+        cv.showsVerticalScrollIndicator = false
+        return cv
+    }()
+
+    private let collectionViewLayout: UICollectionViewLayout = {
+        let minLineSpacing: CGFloat = 1
+        let minInterSpacing: CGFloat = 1
+        let itemWidth = (UIScreen.main.bounds.width - 2) / 2
+        let itemHeight = itemWidth * (248 / 187.0) * 0.5
+
+        let layout = UICollectionViewFlowLayout()
+
+        layout.itemSize = CGSize(width: itemWidth, height: itemHeight)
+        layout.minimumLineSpacing = minLineSpacing
+        layout.minimumInteritemSpacing = minInterSpacing
+        layout.sectionInset = .init(top: 0, left: 0.5, bottom: 0, right: 0.5)
+        return layout
+    }()
+}
+
+extension BookmarkMagazineViewController {
+    private func setUI() {
+        view.addSubview(collectionView)
+
+        collectionView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(5)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
 }
