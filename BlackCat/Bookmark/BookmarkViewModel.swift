cdea271c9286748f36ff3606f66ad2818e11f6fd
SeYeongYoon <ssyoun4092@naver.com>
2022-11-30 17:05:30 +0900

    [추가] editButton 토글 시 subViewModel에 이벤트 방출 로직 추가

--- BlackCat/Bookmark/BookmarkViewModel.swift
+++ BlackCat/Bookmark/BookmarkViewModel.swift
@@ -13,27 +13,46 @@ import RxSwift
 class BookmarkViewModel {
 
     let disposeBag = DisposeBag()
-
-    let bookmarkTattooViewModel = BookmarkTattooViewModel()
+    let bookmarkPageViewModels: [BookmarkTattooViewModel]
+//    let bookmarkTattooViewModel: BookmarkTattooViewModel
+//    let bookmarkMagazineViewModel: BookmarkTattooViewModel
 
     // MARK: - Input
 
     let viewDidLoad = PublishRelay<Void>()
     let didTap = PublishRelay<EditMode>()
     let didTapEditBarButtonItem = PublishRelay<EditMode>()
+    let currentShowingPageIndex = BehaviorRelay<Int>(value: 0)
 
     // MARK: - Output
     let updateModeDriver: Driver<EditMode>
 
-    init() {
+    init(
+//        bookmarkTattooViewModel: BookmarkTattooViewModel,
+//        bookmarkMagazineViewModel: BookmarkTattooViewModel
+        bookmarkPageViewModels: [BookmarkTattooViewModel]
+    ) {
+        self.bookmarkPageViewModels = bookmarkPageViewModels
+
         let didTapEditButton = didTapEditBarButtonItem
             .share()
 
+        let currentShowingPageViewModel = currentShowingPageIndex
+            .distinctUntilChanged()
+            .debug("currentShowingPageIndex")
+            .map { bookmarkPageViewModels[$0] }
+
         didTapEditButton
-            .bind(to: bookmarkTattooViewModel.editMode)
+            .withLatestFrom(currentShowingPageViewModel) { ($0, $1) }
+            .subscribe { editMode, viewModel in
+                print("editMode: \(editMode)")
+                print("viewModel: \(viewModel)")
+                viewModel.editMode.accept(editMode)
+            }
             .disposed(by: disposeBag)
 
-        updateModeDriver = didTapEditButton
+        updateModeDriver = currentShowingPageViewModel
+            .flatMap { $0.editMode }
             .map { $0.toggle() }
             .asDriver(onErrorJustReturn: .edit)
     }
