26d4fc8932212eaf7ffd7979191b1b56576eece6
SeYeongYoon <ssyoun4092@naver.com>
2022-11-30 17:05:56 +0900

    [추가] 편집모드에서 Cell이 이중 클릭했을 때 선택 된 Cell이 취소되는 로직 구현

--- BlackCat/Bookmark/Views/BMTattooCellViewModel.swift
+++ BlackCat/Bookmark/Views/BMTattooCellViewModel.swift
@@ -17,17 +17,32 @@ protocol cellEditable {
 
 class BMTattooCellViewModel: cellEditable {
     var imageURLString: String
-    var selectNumber: String?
-
+    var selectNumber = PublishRelay<Int>()
+    var decreaseSelectNumber = PublishRelay<Void>()
     var showing = PublishRelay<Bool>()
 
     let showEditView: Driver<Bool>
+    let selectNumberText: Driver<String>
 
     init(imageURLString: String) {
         self.imageURLString = imageURLString
 
-//        self.showing = hidden
         showEditView = showing
             .asDriver(onErrorJustReturn: true)
+
+        let currentSelectNumber = selectNumber
+
+        let decreasedCurrentSelectNumber = selectNumber
+            .withLatestFrom(decreaseSelectNumber) { currentNumber, _ in
+                return currentNumber - 1
+            }
+
+        selectNumberText = Observable.merge([
+            currentSelectNumber.asObservable(),
+            decreasedCurrentSelectNumber
+        ])
+            .debug("===Select Number===")
+            .map { String($0) }
+            .asDriver(onErrorJustReturn: "")
     }
 }
