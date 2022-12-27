1e26241d3a145ec7e1103c47cfa64aa5c0b1f8f0
SeYeongYoon <ssyoun4092@naver.com>
2022-12-27 11:36:18 +0900

    [추가] TattooDetailViewController 파일 추가

---
+++ BlackCat/TattooDetail/TattooDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TattooDetailViewController.swift
+//  BlackCat
+//
+//  Created by SeYeong on 2022/12/27.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+import SnapKit
+
+final class TattooDetailViewController: UIViewController {
+
+    // MARK: - Properties
+
+    // MARK: - Binding
+
+    // function
+
+    // MARK: - Initialize
+
+    // MARK: - Life Cycle
+
+    // MARK: - UIComponents
+
+
+}
