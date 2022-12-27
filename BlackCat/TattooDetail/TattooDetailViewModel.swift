174030bdc829f5e8d7669e09236a0ef0ce644ffa
SeYeongYoon <ssyoun4092@naver.com>
2022-12-27 13:14:46 +0900

    [추가] TattooDetail ViewModel 프로퍼티 추가

--- BlackCat/TattooDetail/TattooDetailViewModel.swift
+++ BlackCat/TattooDetail/TattooDetailViewModel.swift
@@ -7,9 +7,28 @@
 
 import UIKit
 
+import BlackCatSDK
 import RxCocoa
 import RxSwift
 
 struct TattooDetailViewModel {
-    
+    private let tattooModel: Model.Tattoo
+
+    let id: Int
+    let price: Int
+    let description: String
+    let liked: Bool
+    let imageURLStrings: [String?]
+    let address: String
+
+    init(tattooModel: Model.Tattoo) {
+        self.tattooModel = tattooModel
+
+        self.id = tattooModel.id
+        self.price = tattooModel.price
+        self.description = tattooModel.description
+        self.liked = tattooModel.liked
+        self.imageURLStrings = tattooModel.imageURLStrings
+        self.address = tattooModel.address
+    }
 }
