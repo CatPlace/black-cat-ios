273fe482eda2cf47e47746d8bb7ece22f414320b
SeYeongYoon <ssyoun4092@naver.com>
2022-12-27 11:37:07 +0900

    [추가] TatooDetailViewModel 추가

---
+++ BlackCat/TattooDetail/TattooDetailViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  TattooDetailViewModel.swift
+//  BlackCat
+//
+//  Created by SeYeong on 2022/12/27.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+struct TattooDetailViewModel {
+    
+}
