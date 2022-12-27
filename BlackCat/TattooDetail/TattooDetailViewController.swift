80ded06159b02194111328675776b412d7428cd3
SeYeongYoon <ssyoun4092@naver.com>
2022-12-27 13:14:30 +0900

    [추가] TattooDetail UI 추가

--- BlackCat/TattooDetail/TattooDetailViewController.swift
+++ BlackCat/TattooDetail/TattooDetailViewController.swift
@@ -7,23 +7,246 @@
 
 import UIKit
 
+import BlackCatSDK
 import RxCocoa
 import RxSwift
 import SnapKit
 
 final class TattooDetailViewController: UIViewController {
 
+    let disposeBag = DisposeBag()
+    enum Reusable {
+        static let tattooDetailCell = ReusableCell<TattooDetailCell>()
+    }
+
     // MARK: - Properties
 
+    let viewModel: TattooDetailViewModel
+
     // MARK: - Binding
+
+    private func bind(to viewModel: TattooDetailViewModel) {
+    }
 
     // function
 
     // MARK: - Initialize
 
+    init(viewModel: TattooDetailViewModel) {
+        self.viewModel = viewModel
+
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setNavigationBar()
+        setUI()
+    }
 
     // MARK: - UIComponents
 
+    private let scrollView = UIScrollView()
 
+    private let contentView = UIView()
+
+    private lazy var flowLayout: UICollectionViewLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 0.0
+        layout.minimumInteritemSpacing = 0.0
+        layout.scrollDirection = .horizontal
+        let width = UIScreen.main.bounds.width
+        let height: CGFloat = cellHeight
+        layout.itemSize = .init(width: width, height: height)
+        return layout
+    }()
+
+    private lazy var collectionView: UICollectionView = {
+        let cv = UICollectionView(frame: .zero,
+                                  collectionViewLayout: flowLayout)
+        cv.register(Reusable.tattooDetailCell)
+        cv.isPagingEnabled = true
+        cv.dataSource = self
+        cv.showsHorizontalScrollIndicator = false
+        return cv
+    }()
+
+    private let shareRightBarButtonItem: UIBarButtonItem = {
+        let image = UIImage(systemName: "square.and.arrow.up")?.withTintColor(.white)
+        $0.image = image
+        return $0
+    }(UIBarButtonItem())
+
+    private let tattooTitle: UILabel = {
+        $0.text = "타투 제목"
+        return $0
+    }(UILabel())
+
+    private let tattooProfileImageView: UIImageView = {
+        $0.layer.cornerRadius = 15
+        $0.clipsToBounds = true
+        $0.backgroundColor = .lightGray
+        return $0
+    }(UIImageView())
+
+    private let tattooistNameLabel: UILabel = {
+        $0.font = .systemFont(ofSize: 16)
+        $0.tintColor = .black
+        $0.text = "김타투"
+        return $0
+    }(UILabel())
+
+    private let dateLabel: UILabel = {
+        $0.font = .systemFont(ofSize: 14)
+        $0.tintColor = .init(hex: "#666666FF")
+        $0.text = "2022년 12월 27일"
+        return $0
+    }(UILabel())
+
+    private let descriptionLabel: UILabel = {
+        $0.font = .systemFont(ofSize: 14)
+        $0.tintColor = .init(hex: "#666666FF")
+        $0.text = " 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n 요즘은 이게 대세입니다.\n "
+        $0.numberOfLines = 0
+        return $0
+    }(UILabel())
+
+    private let blankView = UIView()
+
+    private let askButton: UIButton = {
+        $0.setTitle("문의하기", for: .normal)
+        $0.backgroundColor = .init(hex: "#333333FF")
+        $0.layer.cornerRadius = 20
+        return $0
+    }(UIButton())
+
+    private let bookmarkView: UIView = {
+        $0.backgroundColor = .init(hex: "#333333FF")
+        $0.layer.cornerRadius = 20
+        return $0
+    }(UIView())
+
+    private let heartButton: UIButton = {
+        let heartImage = UIImage(systemName: "heart")?.withTintColor(.white).withRenderingMode(.alwaysOriginal)
+        $0.setImage(heartImage, for: .normal)
+        $0.contentMode = .scaleAspectFit
+        return $0
+    }(UIButton())
+
+    private let bookmarkCountLabel: UILabel = {
+        $0.font = .systemFont(ofSize: 18)
+        $0.adjustsFontSizeToFitWidth = true
+        $0.textColor = .white
+        $0.text = "25"
+        return $0
+    }(UILabel())
+}
+
+extension TattooDetailViewController {
+    private var cellHeight: CGFloat { (500 * UIScreen.main.bounds.width) / 375 }
+
+    private func setNavigationBar() {
+        self.navigationItem.rightBarButtonItem = shareRightBarButtonItem
+    }
+
+    private func setUI() {
+        view.addSubview(scrollView)
+
+        scrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        scrollView.addSubview(contentView)
+
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+
+        [collectionView, tattooTitle, tattooProfileImageView, tattooistNameLabel, dateLabel, descriptionLabel].forEach { contentView.addSubview($0) }
+
+        collectionView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(cellHeight)
+        }
+
+        tattooTitle.snp.makeConstraints {
+            $0.top.equalTo(collectionView.snp.bottom).offset(30)
+            $0.leading.equalToSuperview().inset(30)
+        }
+
+        tattooProfileImageView.snp.makeConstraints {
+            $0.top.equalTo(tattooTitle.snp.bottom).offset(8)
+            $0.leading.equalToSuperview().inset(30)
+            $0.width.height.equalTo(30)
+        }
+
+        tattooistNameLabel.snp.makeConstraints {
+            $0.leading.equalTo(tattooProfileImageView.snp.trailing).offset(5)
+            $0.centerY.equalTo(tattooProfileImageView)
+        }
+
+        dateLabel.snp.makeConstraints {
+            $0.top.equalTo(tattooProfileImageView.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().inset(30)
+        }
+
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(dateLabel.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(30)
+            $0.bottom.equalToSuperview().inset(60)
+        }
+
+        [askButton, bookmarkView].forEach { view.addSubview($0) }
+
+        bookmarkView.snp.makeConstraints {
+            $0.width.equalTo(72)
+            $0.height.equalTo(60)
+            $0.trailing.equalToSuperview().inset(20)
+            $0.bottom.equalToSuperview().inset(28)
+        }
+
+        askButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(20)
+            $0.trailing.equalTo(bookmarkView.snp.leading).offset(-12)
+            $0.height.equalTo(60)
+            $0.bottom.equalToSuperview().inset(28)
+        }
+
+        [heartButton, bookmarkCountLabel].forEach { bookmarkView.addSubview($0) }
+
+        heartButton.snp.makeConstraints {
+            $0.leading.equalTo(14)
+            $0.centerY.equalToSuperview()
+            $0.width.equalTo(20)
+            $0.height.equalTo(18)
+        }
+
+        bookmarkCountLabel.snp.makeConstraints {
+            $0.leading.equalTo(heartButton.snp.trailing).offset(5)
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(14)
+        }
+    }
+}
+
+extension TattooDetailViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        return viewModel.imageURLStrings.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeue(Reusable.tattooDetailCell, for: indexPath)
+        cell.configure(with: viewModel.imageURLStrings[indexPath.row])
+
+        return cell
+    }
 }
