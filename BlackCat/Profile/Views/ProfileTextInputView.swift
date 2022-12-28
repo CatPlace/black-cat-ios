dfb297fc6b09eaae9a232400784c925be2b80e5f
김지훈 <ben224@naver.com>
2022-12-28 18:45:05 +0900

    [수정] 프로필 수정 시 텍스트 입력하는 뷰 수정

--- BlackCat/Profile/Views/ProfileTextInputView.swift
+++ BlackCat/Profile/Views/ProfileTextInputView.swift
@@ -9,10 +9,28 @@ import UIKit
 import RxSwift
 import RxRelay
 import RxCocoa
+import BlackCatSDK
+
+enum ProfileTextInputTitle: String {
+    case 이름, 이메일, 전화번호
+    
+    func info() -> String? {
+        let user = CatSDKUser.user()
+        switch self {
+        case .이름:
+            return user.name
+        case .이메일:
+            return user.email
+        case .전화번호:
+            return user.phoneNumber
+        }
+    }
+}
 
 class ProfileTextInputViewModel {
     // MARK: - Input
-    let inputStringRelay = PublishRelay<String>()
+    let inputStringRelay: BehaviorRelay<String>
+    let viewWillAppearRelay = PublishRelay<Void>()
     
     // MARK: - Output
     let titleDriver: Driver<String>
@@ -20,9 +38,13 @@ class ProfileTextInputViewModel {
     let textCountLimitDriver: Driver<String>
     
     init(title: String, placeholder: String = "텍스트를 입력해주세요", textCountLimit: Int = 18) {
+        inputStringRelay =  .init(value: ProfileTextInputTitle(rawValue: title)?.info() ?? "")
         titleDriver = .just(title)
         placeholderDriver = .just(placeholder)
-        textCountLimitDriver = .just("0/\(textCountLimit)")
+        textCountLimitDriver = inputStringRelay
+            .map { "(\($0.count)/\(textCountLimit))" }
+            .asDriver(onErrorJustReturn: "")
+        
     }
 }
 
@@ -33,6 +55,7 @@ class ProfileTextInputView: UIView {
     // MARK: - Binding
     func bind(to viewModel: ProfileTextInputViewModel) {
         disposeBag.insert {
+            
             profileTextField.rx.text.orEmpty
                 .bind(to: viewModel.inputStringRelay)
             
@@ -43,13 +66,14 @@ class ProfileTextInputView: UIView {
                 .drive(profileTextField.rx.placeholder)
             
             viewModel.textCountLimitDriver
-                .drive(profileTextField.rx.text)
+                .drive(textCountLimitLabel.rx.text)
         }
     }
     
     // MARK: - Initializer
     init(viewModel: ProfileTextInputViewModel) {
         super.init(frame: .zero)
+        profileTextField.delegate = self
         setUI()
         bind(to: viewModel)
     }
@@ -64,19 +88,27 @@ class ProfileTextInputView: UIView {
     
     // MARK: - UIComponents
     let titleLabel: UILabel = {
-        let l = UILabel()
-        return l
-    }()
-    let profileTextField = UITextField()
+        $0.textColor = .init(hex: "#666666FF")
+        $0.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 16)
+        return $0
+    }(UILabel())
+    let profileTextField: UITextField = {
+        $0.setLeftPaddingPoints(10)
+        $0.textColor = .init(hex: "#999999FF")
+        $0.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 12)
+        return $0
+    }(UITextField())
     let textCountLimitLabel: UILabel = {
         let l = UILabel()
-        return l
-    }()
+        $0.textColor = .init(hex: "#999999FF")
+        $0.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 12)
+        return $0
+    }(UILabel())
 }
 
 extension ProfileTextInputView {
     func setUI() {
-        [titleLabel, profileTextField, textCountLimitLabel].forEach { addSubview($0) }
+        [titleLabel, profileTextField].forEach { addSubview($0) }
         
         titleLabel.snp.makeConstraints {
             $0.top.leading.trailing.equalToSuperview()
@@ -88,5 +120,19 @@ extension ProfileTextInputView {
             $0.height.equalTo(48)
             $0.bottom.equalToSuperview()
         }
+        
+        profileTextField.addSubview(textCountLimitLabel)
+        
+        textCountLimitLabel.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(10)
+            $0.centerY.equalToSuperview()
+        }
+    }
+}
+
+extension ProfileTextInputView: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text else { return false }
+        return text.count < 19 || range.length == 1
     }
 }
