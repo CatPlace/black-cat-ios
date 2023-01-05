5f6f2951d89766c2aeeaafd18faffe569270f6fe
김지훈 <ben224@naver.com>
2023-01-06 02:31:59 +0900

    [추가] 로그인 알러트 뷰 추가

---
+++ BlackCat/LoginAlert/LoginAlertViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAlertViewController.swift
+//  BlackCat
+//
+//  Created by 김지훈 on 2023/01/06.
+//
+
+import UIKit
+
+class LoginAlertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
