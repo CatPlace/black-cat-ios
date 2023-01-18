2b8f9260776081c86a48b4e9139dc94a4addb046
김지훈 <ben224@naver.com>
2023-01-18 20:14:18 +0900

    [추가] 가로 스크롤 가능한 뷰컨트롤러 추가
    상속받아서 사용 하세요 ~~

---
+++ BlackCat/Common/InputViews/VerticalScrollableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  VerticalScrollableViewController.swift
+//  BlackCat
+//
+//  Created by 김지훈 on 2023/01/18.
+//
+
+import UIKit
+
+class VerticalScrollableViewController: UIViewController {
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let scrollView = UIScrollView()
+    let contentView = UIView()
+
+    func configure() {
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        scrollView.addSubview(contentView)
+        
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+    }
+}
