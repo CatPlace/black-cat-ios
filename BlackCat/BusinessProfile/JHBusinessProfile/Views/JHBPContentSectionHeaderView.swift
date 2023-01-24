ca8434f49b25ca9417f762c9cdebe35a97345743
김지훈 <ben224@naver.com>
2023-01-25 00:28:12 +0900

    [수정] 타투이스트 페이지 "수정하기" -> "게시글 수정"

--- BlackCat/BusinessProfile/JHBusinessProfile/Views/JHBPContentSectionHeaderView.swift
+++ BlackCat/BusinessProfile/JHBusinessProfile/Views/JHBPContentSectionHeaderView.swift
@@ -118,7 +118,7 @@ class JHBPContentSectionHeaderView: JHBPBaseCollectionReusableView{
         
         func editButtonText() -> String {
             switch self {
-            case .profile: return "수정하기"
+            case .profile: return "게시글 수정"
             case .product: return "타투 업로드"
             case .info: return "견적 수정"
             }
